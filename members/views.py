d7070ea7eb944efcf0e877882e45ea7df122be24
Ann Micheni <60700848+AnnMicheni@users.noreply.github.com>
2024-03-29 08:54:53 +0300

    For Loop

--- members/views.py
+++ members/views.py
@@ -27,7 +27,7 @@ def testing(request):
   context = {
     'fruits': ['Apple', 'Banana', 'Cherry'],   
   }
-  return HttpResponse(template.render(context, request))
+  return HttpResponse(template.render(context, request))  
 
 def custom404(request, exception):
   template = loader.get_template('404.html')
@@ -39,6 +39,7 @@ def testing(request):
     'firstname': 'Ann',
   }
   return HttpResponse(template.render(context,request))
+
 
 def testing(request):
   mymembers = Member.objects.all().values()
@@ -53,5 +54,71 @@ def testing(request):
   context = {
     'x': ['Apple', 'Banana', 'Cherry'], 
     'y': ['Apple', 'Banana', 'Cherry'], 
+  }
+  return HttpResponse(template.render(context, request))
+
+def testing(request):
+  template = loader.get_template('template.html')
+  context = {
+    'cars': [
+      {
+        'brand': 'Ford',
+        'model': 'Mustang',
+        'year': '1964',
+      },
+      {
+        'brand': 'Ford',
+        'model': 'Bronco',
+        'year': '1970',
+      },
+      {
+        'brand': 'Volvo',
+        'model': 'P1800',
+        'year': '1964',
+      }]   
+  }
+  return HttpResponse(template.render(context, request))
+
+def members(request):
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('template.html')
+  context = {
+   'members': [
+  {
+    'id': 1,
+    'firstname': 'Emil',
+    'lastname': 'Refsnes',
+    'phone': 5551234,
+    'joined_date': mymembers.joined_date(2022, 1, 5)
+  },
+  {
+    'id': 2,
+    'firstname': 'Tobias',
+    'lastname': 'Refsnes',
+    'phone': 5557777,
+    'joined_date': mymembers.joined_date(2021, 4, 1)
+  },
+  {
+    'id': 3,
+    'firstname': 'Linus',
+    'lastname': 'Refsnes',
+    'phone': 5554321,
+    'joined_date': mymembers.joined_date(2021, 12, 24)
+  },
+  {
+    'id': 4,
+    'firstname': 'Lene',
+    'lastname': 'Refsnes',
+    'phone': 5551234,
+    'joined_date': mymembers.joined_date(2021, 5, 1)
+  },
+  {
+    'id': 5,
+    'firstname': 'Stalikken',
+    'lastname': 'Refsnes',
+    'phone': 5559876,
+    'joined_date': mymembers.joined_date(2022, 9, 29)
+  }
+],
   }
   return HttpResponse(template.render(context, request))
